26f7d9cb32f4b522dd09caae158108116d14a6ab
ElliottAYoung <elliott.a.young@gmail.com>
2016-03-13 22:10:45 -0500

    Begin work on Simonn

--- lib/simonn.rb
+++ lib/simonn.rb
@@ -1,5 +1,47 @@
 require "simonn/version"
 
 module Simonn
-  # Your code goes here...
+
+  def self.get_read_args(table)
+    puts "How would you like data returned: (all, by query, single)"
+    response = STDIN.gets.chomp
+
+    case response.downcase
+    when "all"
+      QueryBuilder.build_read_query(table)
+    when "by query"
+      puts "Under Construction"
+    when "single"
+      puts "Please enter the id for the #{table} object:"
+      id = STDIN.gets.chomp
+      QueryBuilder.build_read_query(table, {id: id})
+    else
+      puts "Unrecognized response '#{response}': please enter 'all', 'single', or 'by query'"
+    end
+  end
+
+  def self.get_create_args(table)
+  end
+
+  def self.get_update_args(table)
+  end
+
+  def self.get_destroy_args(table)
+  end
+
+end
+
+class QueryBuilder
+
+  def self.build_read_query(table, db_args={})
+  end
+
+end
+
+class Splitter
+
+  def self.prep_args(args)
+    args.split(",")
+  end
+
 end
